8318bc638a2e26516a81ec5d7dd9b47c8a97c7c2
fortune710 <fortune0208@yahoo.com>
2023-01-29 21:36:10 +0100

    navigation to home page added

--- src/pages/Login/login.tsx
+++ src/pages/Login/login.tsx
@@ -1,13 +1,20 @@
 import styles from './login.module.css';
 import Logo from '../../assets/logo.svg';
 import { Button, Input } from '../../Components';
+import { useNavigate } from 'react-router-dom';
 
 export const Login: React.FC = () => {
+  const navigate = useNavigate()
+  
+  const submitForm = (e:any) => {
+    e.preventDefault();
+    navigate('/Home')
+  }
     
   return(
     <main className={styles.Mainpage}>
-      <img src={Logo} alt="" style={{marginBottom:"4rem"}} />
-      <form className={styles.Login}>
+      <img src={Logo} alt="Logo" />
+      <form onSubmit={submitForm} className={styles.Login}>
         <h2 className={styles.header}>Log in to your Account</h2>
         <p className={styles.semitext}>Dont have an account? <span className={styles.blue}>Get started</span></p>
       
@@ -24,7 +31,7 @@ export const Login: React.FC = () => {
           labelName='Password'
         />
 
-        <button className={styles.submit}>
+        <button type='submit' className={styles.submit}>
           Log in
         </button>
       </form>
