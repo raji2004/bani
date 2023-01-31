b2dc054b1deecb7f985e1ec7ad1067a63a9c49f8
Marnin Audu <msmaudu@gmail.com>
2023-02-01 00:43:51 +0100

    Started sidebar

--- src/Components/sidebar/sidebar.tsx
+++ src/Components/sidebar/sidebar.tsx
@@ -1,7 +1,168 @@
-export const Sidebar: React.FC=()=>{
-    return(
+import styles from "./sidebar.module.css";
+import GTLogo from "../../assets/gt.svg";
+import RightArrow from "../../assets/rightCaret.svg";
+import FormatSquare from "../../assets/format-square.svg";
+
+export const Sidebar: React.FC = () => {
+  const d = new Date();
+  let day = d.getDate();
+  let month = d.getMonth();
+  let year = d.getFullYear();
+
+  let suffix;
+  switch (day) {
+    case 1:
+      suffix = "st";
+      break;
+    case 2:
+      suffix = "nd";
+      break;
+
+    default:
+      suffix = "th";
+      break;
+  }
+
+  let monthText;
+  switch (month) {
+    case 0:
+      monthText = "January";
+      break;
+
+    case 1:
+      monthText = "February";
+      break;
+    case 2:
+      monthText = "March";
+      break;
+    case 3:
+      monthText = "April";
+      break;
+    case 4:
+      monthText = "May";
+      break;
+    case 5:
+      monthText = "June";
+      break;
+    case 6:
+      monthText = "July";
+      break;
+    case 7:
+      monthText = "August";
+      break;
+    case 8:
+      monthText = "September";
+      break;
+    case 9:
+      monthText = "October";
+      break;
+    case 10:
+      monthText = "November";
+      break;
+    case 11:
+      monthText = "December";
+      break;
+    default:
+      console.error("Error");
+      break;
+  }
+  return (
+    <div className={styles.sidebar}>
+      <div className={styles.activity}>
+        <span>
+          Activity Feed <i> - </i>{" "}
+          <small style={{ color: "#65717C", fontSize: "0.87em" }}>
+            GT Bank
+          </small>
+        </span>
+
+        <span style={{ display: "flex", flexDirection: "row" }}>
+          <small style={{ marginRight: "0.8125em", color: "#02BA81" }}>
+            Active{" "}
+          </small>
+          <li style={{ fontSize: "0.8125em", color: "#65717C" }}>
+            Created 16days ago
+          </li>
+        </span>
+      </div>
+      <div id={styles.banner}></div>
+      <div className={styles.header}>
         <div>
-            
+          <img
+            style={{
+              borderRadius: "4px",
+              width: "3.25em",
+              marginTop: "-1.3125em",
+            }}
+            src={GTLogo}
+            alt=""
+          />
         </div>
-    )
-}
+        <div className={styles.account_header}>
+          <div>
+            <div>Guaranty Trust Bank</div>
+            <div style={{ color: "#65717C" }}>209630**83</div>
+          </div>
+          <div>
+            <button id={styles.activeViews}>
+              <span>Go to Account View</span>{" "}
+              <img src={RightArrow} style={{ width: "1em" }} alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <hr style={{ marginTop: "5%", width: "100%" }} />
+      <div className={styles.mid}>
+        <div className={styles.mid_labels}>
+          <small
+            style={{
+              color: "#65717C",
+              fontSize: "0.87em",
+            }}
+          >
+            Total Flow
+          </small>
+          <small
+            style={{
+              color: "#65717C",
+              fontSize: "0.87em",
+            }}
+          >
+            Last Activity Date
+          </small>
+          <small
+            style={{
+              color: "#65717C",
+              fontSize: "0.87em",
+            }}
+          >
+            Linked Branch
+          </small>
+        </div>
+
+        <div className={styles.mid_values}>
+          <div>₦77,823.00</div>
+
+          <div style={{ marginLeft: "5%" }}>
+            {day + suffix}
+            {"\u00A0"}
+            {monthText}.{"\u00A0"}
+            {year}
+          </div>
+
+          <div id={styles.linked_branch}>
+            <img src={FormatSquare} alt="" />
+            <span
+              style={{
+                color: "#EA8A15",
+              }}
+            >
+              Dodo - Lekki II
+            </span>
+          </div>
+        </div>
+      </div>
+      <hr style={{ marginTop: "5%", width: "100%" }} />
+    </div>
+  );
+};
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,92 +1,90 @@
-import styles from './home.module.css'
-import logo from '../../assets/logo.svg'
-import notifications from '../../assets/notifications.svg'
-import profile from '../../assets/profile.svg'
-import acc from "../../assets/acc.svg"
-import minfo from "../../assets/minfo.svg"
-import manage from '../../assets/manage.svg'
-import {  BankInfo, Dashboard, Nav } from '../../Components';
-import { Banks } from './banks'
-import { useId, useState } from 'react'
+import styles from "./home.module.css";
+import logo from "../../assets/logo.svg";
+import notifications from "../../assets/notifications.svg";
+import profile from "../../assets/profile.svg";
+import acc from "../../assets/acc.svg";
+import minfo from "../../assets/minfo.svg";
+import manage from "../../assets/manage.svg";
+import { BankInfo, Dashboard, Nav } from "../../Components";
+import { Banks } from "./banks";
+import { Sidebar } from "../../Components";
+import { useId, useState } from "react";
 
 export const Home: React.FC = () => {
-    const [activeId, setActiveId] = useState<string>('')
-    const [activeBank, setActiveBank] = useState<any>(undefined)
+  const [activeId, setActiveId] = useState<string>("");
+  const [activeBank, setActiveBank] = useState<any>(undefined);
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.Nav}>
-                <img src={logo} alt="t" />
-                <div className={styles.profile}>
-                    <img src={notifications} alt="" />
-                    <img src={profile} alt="" />
-                </div>
-            </div>
-            <div className={styles.main}>
-                <Nav />
-                <div className={styles.balance}>
-                    <Dashboard />
-                    <div className={styles.myinfo}>
-                        <div className={styles.acc}>
-                            <img src={acc} alt="" style={{ width: '100%' }} />
-
-                            <div className={styles.minfo}>
-                                <img src={minfo} alt="" style={{ width: '100%' }} />
-                                <div className={styles.title}>
-                                    <h4>Lekki II</h4>
-
-                                    <img src={manage} alt="" style={{ width: '14%' }} />
-                                </div>
-                                <div className={styles.td}>
-                                    <p style={{ marginLeft: -77 }}>Bank Name</p>
-                                    <p style={{ marginLeft: 10 }}>Virtual Account</p>
-                                    <p>Total Inflow</p>
-                                </div>
-                                {/* turn to components */}
-                                {
-                                    Banks.map(bank => {
-                                        const id = useId();
-                                        return(
-                                            <BankInfo
-                                                key={id}
-                                                onClick={() => setActiveBank(bank)}
-                                                active={activeBank === bank ? true : false}
-                                                bankName={bank.name}
-                                                bankLogo={bank.bankLogo}
-                                                bankLocation={bank.bankLocation}
-                                            />
-                                        )
-                                    })
-                                }
-                            </div>
-                        </div>
-                        {
-                            !activeBank ? null :
-                            <div className={styles.rightContainer}>
-                                <table>
-                                    <thead>
-                                        <td>Transaction Details</td>
-                                        <td>Amount</td>
-                                        <td>Date</td>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Olaoluwa S.</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Olaoluwa S.</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Olaoluwa S.</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                        }
-                    </div>
-                </div>
-
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.Nav}>
+        <img src={logo} alt="t" />
+        <div className={styles.profile}>
+          <img src={notifications} alt="" />
+          <img src={profile} alt="" />
         </div>
-    )
-}
+      </div>
+      <div className={styles.main}>
+        <Nav />
+        <div className={styles.balance}>
+          <Dashboard />
+          <div className={styles.myinfo}>
+            <div className={styles.acc}>
+              <img src={acc} alt="" style={{ width: "100%" }} />
+
+              <div className={styles.minfo}>
+                <img src={minfo} alt="" style={{ width: "100%" }} />
+                <div className={styles.title}>
+                  <h4>Lekki II</h4>
+
+                  <img src={manage} alt="" style={{ width: "14%" }} />
+                </div>
+                <div className={styles.td}>
+                  <p style={{ marginLeft: -77 }}>Bank Name</p>
+                  <p style={{ marginLeft: 10 }}>Virtual Account</p>
+                  <p>Total Inflow</p>
+                </div>
+                {/* turn to components */}
+                {Banks.map((bank) => {
+                  const id = useId();
+                  return (
+                    <BankInfo
+                      key={id}
+                      onClick={() => setActiveBank(bank)}
+                      active={activeBank === bank ? true : false}
+                      bankName={bank.name}
+                      bankLogo={bank.bankLogo}
+                      bankLocation={bank.bankLocation}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+            {!activeBank ? null : (
+              <div className={styles.rightContainer}>
+                <table>
+                  <thead>
+                    <td>Transaction Details</td>
+                    <td>Amount</td>
+                    <td>Date</td>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Olaoluwa S.</td>
+                    </tr>
+                    <tr>
+                      <td>Olaoluwa S.</td>
+                    </tr>
+                    <tr>
+                      <td>Olaoluwa S.</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            )}
+            <Sidebar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
